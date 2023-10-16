973ec409f4de101db54558e8e4c1956ec42c1d16
Neilander <130475002+Neilander@users.noreply.github.com>
2023-10-16 18:36:19 -0400

    对话和交互
    dialogue是测试过了的，里面有测试部分到实际project里面再删除；交互系统缺少玩家和交互环境，还未测试

---
+++ Scripts/dialogueManager.cs
@@ -0,0 +1,214 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+
+
+
+public class dialogueManager : MonoBehaviour
+{
+    public GameObject leftObject;
+    public GameObject rightObject;
+    public static GameObject leftImage = null;
+    public static GameObject rightImage = null;
+    public static dialogueManager dManager;
+
+    
+    public TextMeshProUGUI dialogueText;
+    public Dialogue d = new Dialogue();
+
+    private Queue<string> sentences = new Queue<string>();
+    private Dialogue curDialogue = null;
+    private int curIndex = 0;
+
+    private bool inCoroutine = true;//the bool to stop coroutine
+    private bool running = false;//the bool to detect if coroutine is running
+
+    private void Awake()
+    {
+        leftImage = leftObject;
+        rightImage = rightObject;
+        dManager = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //for test
+        spriteModifier sMod1 = new spriteModifier(1,
+                                                (GameObject gm) => { gm.SetActive(true); },
+                                                (GameObject gm) => { gm.SetActive(true); });
+        spriteModifier sMod2 = new spriteModifier(3,
+                                                _rm: (GameObject gm) => { SetImageOpacity(gm.GetComponent<Image>(), 0.2f); });
+        spriteModifier sMod3 = new spriteModifier(4,
+                                                 (GameObject gm) => { gm.GetComponent<Image>().color = Color.gray; },
+                                                 (GameObject gm) => { SetImageOpacity(gm.GetComponent<Image>(), 1f); });
+        d.SetModQue(new Queue<spriteModifier>(new[] { sMod1, sMod2, sMod3}));
+        StartDialogue(d);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (curDialogue != null)
+            {
+                nextSentence();
+            }
+        }
+    }
+
+    public void StartDialogue(Dialogue _d)
+    {
+
+        //没有加角色禁用的部分，同理没有加解锁部分
+        curDialogue = _d;
+        curIndex = 0;
+        dialogueText.enabled = true;
+
+        sentences.Clear();
+        foreach (string sentence in _d.text)
+        {
+            sentences.Enqueue(sentence);
+        }
+        if (sentences.Count != 0)
+        {
+            nextSentence();
+        }
+
+    }
+
+    private void nextSentence()
+    {
+        if (running)
+        {
+            running = false;
+            inCoroutine = false;
+        }
+        else
+        {
+            if (curIndex == curDialogue.text.Length)
+            {
+                EndSentence();
+                return;
+            }
+            inCoroutine = true;
+            string curText = sentences.Dequeue();
+            //display
+            StartCoroutine(TypeSentence(curText, curDialogue, curIndex));
+            running = true;
+            curIndex += 1;
+        }
+    }
+
+    private void EndSentence()
+    {
+        leftImage.SetActive(false);
+        rightImage.SetActive(false);
+        curDialogue = null;
+        dialogueText.enabled = false;
+    }
+
+    IEnumerator TypeSentence(string sentence, Dialogue d, int index)
+    {
+        d.checkDialogue(index);
+        dialogueText.text = "";
+
+        foreach (char letter in sentence.ToCharArray())
+        {
+            if (inCoroutine)
+            {
+                dialogueText.text += letter;
+                yield return new WaitForSeconds(0.05f);
+            }
+            else
+            {
+                dialogueText.text = sentence;
+                break;
+            }
+
+        }
+        running = false;
+    }
+
+    public static void SetImageOpacity(Image img, float opacity)
+    {
+        if (img != null)
+        {
+            Color currentColor = img.color;
+            currentColor.a = opacity;
+            img.color = currentColor;
+            
+        }
+    }
+}
+
+
+
+//用来代表角色对话的类
+[System.Serializable]
+public class Dialogue
+{
+    //角色的名字和内容
+    [TextArea(3,10)]
+    public string[] text = null;//每句话包含说话者， Neil:....
+    public Sprite sprLeft = null;//说话时左右的立绘和
+    public Sprite sprRight = null;
+
+
+    private Queue<spriteModifier> modQue = new Queue<spriteModifier>();
+
+    public Dialogue(Queue<spriteModifier> _mQ = null)
+    {
+        modQue = _mQ;
+    }
+
+    public void checkDialogue(int counter)
+    {
+        if (modQue!= null && modQue.Count > 0)
+        {
+            if (counter == modQue.Peek().pointer)
+            {
+                modQue.Dequeue().modifySpr();
+            }
+        }
+    }
+
+    public void SetModQue(Queue<spriteModifier> qsm) { modQue = qsm; }
+}
+
+
+//这种委托代表了对于一个特定的UI Image组件进行调试
+public delegate void modSpr(GameObject uiSpr);
+
+//这个类代表了，在dialogue进行到指针处时，可以执行modifySpr方法，
+public class spriteModifier
+{
+    public int pointer;
+    private modSpr leftModification;
+    private modSpr rightModification;
+
+    public spriteModifier(int _p, modSpr _lm = null, modSpr _rm = null)
+    {
+        pointer = _p;
+        leftModification = _lm;
+        rightModification = _rm;
+    }
+
+
+    //参考中功能为变亮变暗，所以现在为瞬间modify
+    public void modifySpr()
+    {
+        if (leftModification != null) { leftModification(dialogueManager.leftImage); }
+        if (rightModification != null) { rightModification(dialogueManager.rightImage); }
+
+    }
+}
+
+
+
---
+++ Scripts/interactionManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactionManager : MonoBehaviour
+{
+
+    //并未测试
+    public static interactionManager iManager;
+    public GameObject player;
+    private List<Interactable> potentialInteractions = new List<Interactable>();
+
+    private void Awake()
+    {
+        iManager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (potentialInteractions.Count != 0)
+        {
+            disPlayInteraction(potentialInteractions[0]);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                potentialInteractions[0].triggerDialogue();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (potentialInteractions.Count > 1)
+        {
+            sortInteractions(player.transform.position);
+        }
+    }
+
+    //这个只需要把最近的放到最前面就行
+    private void sortInteractions(Vector2 centerPos)
+    {
+        potentialInteractions.Sort((a, b) =>  Vector2.Distance(centerPos, a.returnPos()).CompareTo(Vector2.Distance(centerPos, b.returnPos())));
+    }
+
+    public void connectInteraction(Interactable pI)
+    {
+        potentialInteractions.Add(pI);
+        sortInteractions(player.transform.position);
+    }
+
+    public void disconnect(Interactable pI)
+    {
+        potentialInteractions.Remove(pI);
+        sortInteractions(player.transform.position);
+    }
+
+    private void disPlayInteraction(Interactable i)
+    {
+
+    }
+}
