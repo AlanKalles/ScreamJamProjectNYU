273079577b68540a206ebfc09f85bb04279f56e6
AlanKalles <104079113+AlanKalles@users.noreply.github.com>
2023-10-22 21:06:30 -0400

    save & load

---
+++ ScreamJam/Assets/Script/PlayerData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData 
+{
+ 
+    public int progress;
+    public float[] playerPosition;
+
+        
+    public PlayerData(PlayerControl player)
+    {
+        
+    }
+}
