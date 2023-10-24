689fc0bc021789db0e5972aa5799ba34f969ffa4
AlanKalles <104079113+AlanKalles@users.noreply.github.com>
2023-10-24 04:18:19 -0400

    final

--- ScreamJam/Assets/Script/ToggleMusic.cs
+++ ScreamJam/Assets/Script/ToggleMusic.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEditorInternal.VersionControl.ListControl;
 
 public class ToggleMusic : MonoBehaviour
 {
